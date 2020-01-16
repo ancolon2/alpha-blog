a5206756b5602b5cd96eedef70a6be69601d037e
ancolon2 <andrew.colon@yahoo.com>
2020-01-16 18:34:51 +0000

    Updated DB schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200115142706) do
+ActiveRecord::Schema.define(version: 20200115142134) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
